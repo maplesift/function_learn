415e9c6c759c912031d81b61e47408dcf4400156
maplesift <a23050716@gmail.com>
2025-01-11 20:23:10 +0800

    fun練1/11

--- db copy.php
+++ db copy.php
@@ -1,10 +1,10 @@
 <?php
 
 class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db003";
-    protected $table;
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db03";
     protected $pdo;
-    
+    protected $table;
+
     function __construct($table){
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,'root','');
@@ -14,79 +14,95 @@ class DB{
         foreach($array as $key => $val){
             $tmp[]="`$key`='$val'";
         }
+
         return $tmp;
     }
-    protected function fetchAll($sql){
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
+
     protected function fetchOne($sql){
+        //echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
+    protected function fetchAll($sql){
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     function all(...$arg){
-        $sql=" SELECT * FROM $this->table ";
+        $sql="SELECT * FROM $this->table ";
         if(!empty($arg[0])){
             if(is_array($arg[0])){
+
                 $where=$this->a2s($arg[0]);
-                $sql .= " WHERE ".join( " && ",$where );
+                $sql=$sql . " WHERE ". join(" && ",$where);
             }else{
                 $sql .= $arg[0];
             }
         }
         if(!empty($arg[1])){
-            $sql .= $arg[1];
+            $sql=$sql . $arg[1];
         }
         return $this->fetchAll($sql);
-         
     }
+
     function find($id){
-        $sql=" SELECT * FROM $this->table ";
+        $sql="SELECT * FROM $this->table ";
+
         if(is_array($id)){
             $where=$this->a2s($id);
-            $sql .= " WHERE ".join( " && ",$where );
+            $sql=$sql . " WHERE ". join(" && ",$where);
         }else{
-            $sql .= "WHERE `id`='$id'";
+            $sql .= " WHERE `id`='$id' ";
         }
 
         return $this->fetchOne($sql);
     }
-    
+
     function del($id){
-        // delete
-        $sql=" DELETE  FROM $this->table";
+        $sql="DELETE FROM $this->table ";
+
         if(is_array($id)){
             $where=$this->a2s($id);
-            $sql .= " WHERE ".join( " && ",$where );
+            $sql=$sql . " WHERE ". join(" && ",$where);
         }else{
-            $sql .= " WHERE `id`='$id'";
+            $sql .= " WHERE `id`='$id' ";
         }
         return $this->pdo->exec($sql);
     }
-
+    
     function save($array){
-            // update
         if(isset($array['id'])){
-            $id=$array['id']; 
+            //update
+            //update table set `欄位1`='值1',`欄位2`='值2' where `id`='值' 
+            $id=$array['id'];
             unset($array['id']);
             $set=$this->a2s($array);
-            $sql="UPDATE $this->table SET ".join(",",$set)." WHERE `id`='$id'";
+            $sql ="UPDATE $this->table SET ".join(',',$set)." where `id`='$id'";
         }else{
-            // insert into
-            $col=array_keys($array);
-            $sql="INSERT INTO $this->table (`".join("`,`",$col)."`) VALUES('".join("','",$array)."')";
+            //insert
+            $cols=array_keys($array);
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
         }
+
         return $this->pdo->exec($sql);
     }
+    
 
-    protected function math($math,$col='id',$where=[]){
-        $sql="SELECT $math($col) FROM $this->table ";
+
+     protected function math($math,$col='id',$where=[]){
+        $sql="SELECT $math($col) FROM $this->table";
         if(!empty($where)){
             $tmp=$this->a2s($where);
-            $sql .= " WHERE " .join(" && ",$tmp);
+            $sql=$sql . " WHERE " . join(" && ", $tmp);
         }
+
         return $this->pdo->query($sql)->fetchColumn();
     }
-    function sum($col,$where){
+
+    function max($col,$where=[]){
+        return $this->math('max',$col,$where);
+    }
+    function sum($col,$where=[]){
         return $this->math('sum',$col,$where);
     }
     function count($where=[]){
@@ -94,16 +110,19 @@ class DB{
     }
 
 }
-// db外
+
 function q($sql){
-    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db003",'root','');
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db03",'root','');
+    // echo $sql;
     return $pdo->query($sql)->fetchAll();
 }
-function to($url){
-    header("location:".$url);
-}
+
 function dd($array){
     echo "<pre>";
     print_r($array);
     echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
 }
