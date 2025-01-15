16bac0631ed2c05645020b2eba8cd046d108bfdb
maplesift <a23050716@gmail.com>
2025-01-15 16:28:23 +0800

    1/15 fun練

--- fun02.php
+++ fun02.php
@@ -9,23 +9,89 @@ class DB{
         $this->pdo=new PDO($this->dsn,'root','');
     }
     function a2s($array){
-        
+        $tmp=[];
+        foreach ($array as $key => $val) {
+            $tmp[]="`$key`='$val'";
+        }
+        return $tmp;
     }
 
     protected  function fetchAll($sql){
-
+        return $this->pdo->query($sql)->fetchAll(PDO::FECTH_ASSOC);
     }
     protected  function fetchOne($sql){
-
+        return $this->pdo->query($sql)->fetch(PDO::FECTH_ASSOC);
     }
+    function all(...$arg){
+        $sql="SELECT * FROM $this->table";
+        if(!empty($arg[0])){
+            if(is_array($arg[0])){
+                $where=$this->a2s($arg[0]);
+                $sql .= " WHERE ".join(" && ",$where); 
+            }
+        }if(!empty($arg[1])){
+            $sql .= $arg[1];
+        }
+        return $this->fetchAll($sql);
+    }
+    function find($id){
+        $sql="SELECT * FROM $this->table";
+            if(is_array($id)){
+                
+                $where=$this->a2s($id);
+                $sql .= " WHERE ".join(" && ",$where); 
+            }else{
+                $sql .= "WHERE `id`='$id'";
+            }
+
+        return $this->fetchOne($sql);
+    }
+    
+    function del($id){
+        $sql="DELETE  FROM $this->table";
+            if(is_array($id)){
+                
+                $where=$this->a2s($id);
+                $sql .= " WHERE ".join(" && ",$where); 
+            }else{
+                $sql .= "`id`='$id'";
+            }
+
+        return $this->pdo->exec($sql);
+    }
+    
+
+    function save($array){
+        // update
+        if(isset($array['id'])){
+            $id=$array['id'];
+            unset($array['id']);
+            $set=$this->a2s($array);
+            $sql="UPDATE $this->table SET".join(',',$array)." where `id`='$id'";
+        }else{
+            // insert into
+            $cols=array_keys($array);
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+        }
+            return $this->pdo->exec($sql);
+    }
+
+    
+    protected function math(){}
+    function sum(){}
+    function count(){}
+
 }
 // db外
-function to(){
-
+function to($url){
+    header("location:".$url);
 }
-function dd(){
-
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
 }
-function q(){
-    
+function q($sql){
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db03",'root','');
+    return $pdo->query($sql)->fetchAll();
 }
